d7c9cf6aa5c483d4c1087f3e42d763295523a9e7
史孝开 <shixiaokai@chinamobile.com>
2021-05-26 21:03:33 +0800

    feat(springcloud):
    sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atme/springcloud/controller/RateLimitController.java
@@ -0,0 +1,39 @@
+package com.atme.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entity.CommonResult;
+import com.atguigu.springcloud.entity.Payment;
+import com.atme.springcloud.handler.CustomerBlockHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wsk
+ * @date 2020/3/24 21:42
+ */
+@RestController
+public class RateLimitController {
+
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource", blockHandler = "handlerException")
+    public CommonResult byResource() {
+        return new CommonResult(200, "按资源名称限流测试OK", new Payment(2020L, "serial001"));
+    }
+
+    /**
+     *
+     * 备用方法兜底
+     */
+    public CommonResult handlerException(BlockException exception) {
+        return new CommonResult(444, exception.getClass().getCanonicalName() + "\t 服务不可用");
+    }
+
+    @GetMapping("/rateLimit/customerBlockHandler")
+    @SentinelResource(value = "customerBlockHandler",
+            blockHandlerClass = CustomerBlockHandler.class,
+            blockHandler = "handlerException2")
+    public CommonResult customerBlockHandler() {
+        return new CommonResult(200, "按客户自定义", new Payment(2020L, "serial002"));
+    }
+}

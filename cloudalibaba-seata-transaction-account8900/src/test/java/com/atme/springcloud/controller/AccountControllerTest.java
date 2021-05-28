ecd4a59f061734b50fcdd81ee0117c240686fc57
史孝开 <shixiaokai@chinamobile.com>
2021-05-29 01:30:03 +0800

    修改seata配置文件

---
+++ cloudalibaba-seata-transaction-account8900/src/test/java/com/atme/springcloud/controller/AccountControllerTest.java
@@ -0,0 +1,47 @@
+package com.atme.springcloud.controller;
+
+import com.atme.springcloud.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AccountController.class)
+public class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AccountService mockAccountService;
+
+    @Test
+    public void testDecrease() throws Exception {
+        // Setup
+
+        // Run the test
+        final MockHttpServletResponse response = mockMvc.perform(get("/account/decrease")
+                .param("userId", "0")
+                .param("money", "0")
+                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Verify the results
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals("expectedResponse", response.getContentAsString());
+        verify(mockAccountService).decrease(0L, new BigDecimal("0.00"));
+    }
+}
